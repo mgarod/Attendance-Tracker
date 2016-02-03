1b6f3d609b41d6b06763b494eb82953795d87a84
Igor Goldvekht <goldvekht.igor@gmail.com>
2016-02-02 23:12:03 -0500

    Add ClassYear getter via String

--- Information/ClassYear.java
+++ Information/ClassYear.java
@@ -1,5 +1,16 @@
 package Information;
 
 public enum ClassYear {
-    FRESHMAN, SOPHOMORE, JUNIOR, SENIOR, POST_BACC, NON_DEGREE, OTHER
+    FRESHMAN, SOPHOMORE, JUNIOR, SENIOR, POST_BACC, NON_DEGREE, OTHER;
+
+    ClassYear() {}
+
+    public static ClassYear getClassYear(String classYear) {
+        for (ClassYear cy : ClassYear.values()) {
+            if (cy.name().equals(classYear)) {
+                return cy;
+            }
+        }
+        throw new IllegalArgumentException(classYear + " is not a valid ClassYear");
+    }
 }
